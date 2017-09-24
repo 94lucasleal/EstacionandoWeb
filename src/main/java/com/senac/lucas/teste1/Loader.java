19c8e8dbf2b2e1ee534ea1554733d83115132d1d
94lucasleal <94lucasleal@users.noreply.github.com>
2017-09-24 14:53:34 -0300

    deploying

--- src/main/java/com/senac/lucas/teste1/Loader.java
+++ src/main/java/com/senac/lucas/teste1/Loader.java
@@ -17,19 +17,19 @@ public class Loader {
         String port = System.getenv("PORT");
         String host = "http://0.0.0.0:";
         String uri = "/"+SimpleService.class.getSimpleName();
-        SimpleService service = new SimpleService();
-        Endpoint.publish(host + port + uri,service);
+        //SimpleService service = new SimpleService();
+        //Endpoint.publish(host + port + uri,service);
               
         uri = "/"+CartaoRN.class.getSimpleName();
         CartaoRN cartaoRN = new CartaoRN();
         Endpoint.publish(host + port + uri,cartaoRN);
         
-        uri = "/"+UsuarioRN.class.getSimpleName();
-        UsuarioRN usuarioRN = new UsuarioRN();
-        Endpoint.publish(host + port + uri,usuarioRN);
+        //uri = "/"+UsuarioRN.class.getSimpleName();
+        //UsuarioRN usuarioRN = new UsuarioRN();
+        //Endpoint.publish(host + port + uri,usuarioRN);
         
-        uri = "/"+EstabelecimentoRN.class.getSimpleName();
-        EstabelecimentoRN estabelecimentoRN = new EstabelecimentoRN();
-        Endpoint.publish(host + port + uri,estabelecimentoRN);  
+        //uri = "/"+EstabelecimentoRN.class.getSimpleName();
+        //EstabelecimentoRN estabelecimentoRN = new EstabelecimentoRN();
+        //Endpoint.publish(host + port + uri,estabelecimentoRN);  
     }
 }
