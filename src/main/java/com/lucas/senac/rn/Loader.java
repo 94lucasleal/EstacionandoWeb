8b775f0f6c5f0fe92d99bfe1f49beda59bf9ebf8
94lucasleal <94lucasleal@users.noreply.github.com>
2017-09-24 15:36:58 -0300

    deploying

--- src/main/java/com/lucas/senac/rn/Loader.java
+++ src/main/java/com/lucas/senac/rn/Loader.java
@@ -16,22 +16,22 @@ public class Loader {
         String host = "http://0.0.0.0:";
         String uri = "/"+SimpleService.class.getSimpleName();
         SimpleService service = new SimpleService();
-        //Endpoint.publish(host + port + uri,service);
+        Endpoint.publish(host + "1234" + uri,service);
               
         port = System.getenv("PORT");
         uri = "/"+CartaoRN.class.getSimpleName();
         CartaoRN cartaoRN = new CartaoRN();
-        Endpoint.publish(host + port + uri,cartaoRN);
+        Endpoint.publish(host + "1235" + uri,cartaoRN);
         
         port = System.getenv("PORT");
         uri = "/"+UsuarioRN.class.getSimpleName();
         UsuarioRN usuarioRN = new UsuarioRN();
-        Endpoint.publish(host + port + uri,usuarioRN);
+        Endpoint.publish(host + "1236" + uri,usuarioRN);
         
         port = System.getenv("PORT");
         uri = "/"+EstabelecimentoRN.class.getSimpleName();
         EstabelecimentoRN estabelecimentoRN = new EstabelecimentoRN();
-        Endpoint.publish(host + port + uri,estabelecimentoRN); 
+        Endpoint.publish(host + "1237" + uri,estabelecimentoRN); 
         
     }
 }
--- src/main/java/com/lucas/senac/rn/SimpleService.java
+++ src/main/java/com/lucas/senac/rn/SimpleService.java
@@ -8,7 +8,7 @@ import javax.jws.WebService;
  * @version 0.1
  */
 @WebService(endpointInterface = "com.lucas.senac.rn.SimpleService", serviceName = "SimpleService")
-public class SimpleService {
+public class SimpleService{
 
     public String hello (String name){
         return "Hello, "+name+"!";
