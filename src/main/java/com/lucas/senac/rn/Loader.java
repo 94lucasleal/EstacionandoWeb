4630632f19742b779ac485537266647f7330f347
94lucasleal <94lucasleal@users.noreply.github.com>
2017-09-24 15:24:15 -0300

    deploying

--- src/main/java/com/lucas/senac/rn/Loader.java
+++ src/main/java/com/lucas/senac/rn/Loader.java
@@ -2,6 +2,7 @@ package com.lucas.senac.rn;
 
 import java.net.MalformedURLException;
 import javax.xml.ws.Endpoint;
+import javax.xml.ws.WebServiceFeature;
 
 /**
  *
@@ -17,18 +18,20 @@ public class Loader {
         SimpleService service = new SimpleService();
         //Endpoint.publish(host + port + uri,service);
               
+        port = System.getenv("PORT");
         uri = "/"+CartaoRN.class.getSimpleName();
         CartaoRN cartaoRN = new CartaoRN();
         Endpoint.publish(host + port + uri,cartaoRN);
         
+        port = System.getenv("PORT");
         uri = "/"+UsuarioRN.class.getSimpleName();
         UsuarioRN usuarioRN = new UsuarioRN();
         Endpoint.publish(host + port + uri,usuarioRN);
         
-        /*
+        port = System.getenv("PORT");
         uri = "/"+EstabelecimentoRN.class.getSimpleName();
         EstabelecimentoRN estabelecimentoRN = new EstabelecimentoRN();
         Endpoint.publish(host + port + uri,estabelecimentoRN); 
-        */
+        
     }
 }
